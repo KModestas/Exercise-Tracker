b79a03491b8eeec878ee85a656807f9f25861c5c
Modestas <modestask97@gmail.com>
2020-01-23 11:36:19 +0000

    comments

--- backend/models/exercise.model.js
+++ backend/models/exercise.model.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+// define the schema
 const exerciseSchema = new Schema(
   {
     // instead of required true, you can return a string message as a validation message
@@ -15,6 +16,7 @@ const exerciseSchema = new Schema(
   }
 );
 
+// create a mongoose model, wrapping the schema and providing an interface to the db for creating, querying, updating, deleting records
 const Excerise = mongoose.model("Excerise", exerciseSchema);
 
 module.exports = Excerise;
