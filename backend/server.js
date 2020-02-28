42d60e92ec20098a20a23e6fce77469e308493b1
Modestas <modestask97@gmail.com>
2020-02-28 14:32:41 +0000

    async err

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
 
 // built int http object from node
 // const http = require('http')
@@ -8,37 +8,37 @@ const mongoose = require("mongoose");
 // console.log(http.METHODS)
 // console.log(http.STATUS_CODES)
 
-const exerciseRouter = require("./routes/exercises");
-const userRouter = require("./routes/users");
+const exerciseRouter = require('./routes/exercises')
+const userRouter = require('./routes/users')
 
 // loads environment variables from a .env file into process.env.
-require("dotenv").config();
+require('dotenv').config()
 
-const app = express();
-const port = process.env.PORT || 5000;
+const app = express()
+const port = process.env.PORT || 5000
 // app.use middle ware allows you to specify a function that will run before every request
 // app.use passes req, res and next to every function - next is important
-app.use(cors());
+app.use(cors())
 // parses the body of requests (req.body) into json because often body data may be encoded or binary etc
-app.use(express.json());
+app.use(express.json())
 
 // connect to mongoAtlas DB using the connection string which we get from mongoAtlas site
-const uri = process.env.ATLAS_URI;
+const uri = process.env.ATLAS_URI
 // useNewUrlParser etc are there to prevent breaking changes, not improtant to know about
 mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true
-});
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true
+})
 // you can use this then block or connection.open below to handle the promise when the db connects
 //   .then(() => console.log("MongoDB connected!"))
 //   .catch(err => console.log("Error", err.message));
 
-const connection = mongoose.connection;
-connection.once("open", () => console.log("MongoDB connected!"));
+const connection = mongoose.connection
+connection.once('open', () => console.log('MongoDB connected!'))
 
 // define the endpoints of your app, you can have a single endpoint here like /api and pass in a file that contains all routes
-app.use("/exercises", exerciseRouter);
-app.use("/users", userRouter);
+app.use('/exercises', exerciseRouter)
+app.use('/users', userRouter)
 
-app.listen(port, () => console.log("SERVER RUNNING"));
+app.listen(port, () => console.log('SERVER RUNNING'))
